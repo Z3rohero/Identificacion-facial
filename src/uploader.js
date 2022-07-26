9152d8369126503f5ee042232e9a474a6c49d4e6
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-07-26 14:42:44 +0000

    x

--- src/uploader.js
+++ src/uploader.js
@@ -1,18 +1,28 @@
 "use strict";
 
-const uploaderfile = file =>{
-  
+const uploadFile = file => {
+
+  window.requestFileSystem(window.TEMPORARY, 1024 * 1024, function(fs) {
+    fs.root.getFile(`${file.name}${uuidv4()}`, { create: true, exclusive: true }, function(fileEntry) {
+      fileEntry.createWriter(function(fileWriter) {
+        fileWriter.write(file);
+        resolve(fileEntry);
+      }, e => console.log(e));
+    }, e => console.log(e));
+  })
+
 }
 
-const uploader =(submitSelector,imagesListSelector)=>{
+
+const uploader = (submitSelector, imagesListSelector) => {
 
   const submit = document.querySelector(submitSelector);
   const imagenlist = document.querySelector(imagesListSelector);
   //evento de envio
-  submit.addEvenListener('change',e =>{
-     uploaderfile(e.target.files[0]);
+  submit.addEvenListener('change', e => {
+    uploadFile(e.target.files[0]);
   })
-  
+
 }
 
 
